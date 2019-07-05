97ed91c7d1c87f8e6493e3d2534aacf95cec4ddd
Jake Knecht <Ljknecht91@gmail.com>
2019-07-05 13:46:18 -0500

    edited for Heroku deployment

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,13 +3,41 @@ const pg = require( 'pg' );
 
 //globals
 const Pool = pg.Pool;
-const config ={
-    database: 'inventory',
+
+// Both Local and Heroku database config
+let config ={}
+
+if ( process.env.DATABASE_URL){
+        // Heroku config
+        const url = require('url');
+        const params = url.parse ( process.env.DATABASE_URL );
+        const auth = params.auth.split(':');
+
+        config = {
+        // heroku puts security on the database
+            user: auth[0],
+            password: auth[1],
+        // need to get remote server and port
+            host: params.host,
+            port: params.port,
+        // get the database name by splitting the pathname
+        // and taking the 2nd part (array item 1, second in list)
+            database: params.pathname.split('/')[1],
+        // last two items don't change
+            max: 12,
+            idleTimeOutMillis: 30000
+        }
+}
+else {
+    // local config set up   
+  config = {
+    database: 'playfair_inventory',
     host: 'localhost',
     port: 5432,
     max: 12,
     idleTimeOutMillis: 30000
-} //end config
+           } 
+}//end config
 const pool = new Pool( config );
 
 // db connection
